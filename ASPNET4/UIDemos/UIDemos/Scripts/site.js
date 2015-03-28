aab6d6fd82503cf80ab0cacd0cbe63def137a6bb
Dustin Ewers <dustin.ewers@gmail.com>
2015-03-27 21:56:12 -0500

    JS Fix + slide updates

--- ASPNET4/UIDemos/UIDemos/Scripts/site.js
+++ ASPNET4/UIDemos/UIDemos/Scripts/site.js
@@ -4,22 +4,23 @@
 			format: "MM/DD/YYYY"
 		});
 	});
-
-	jQuery.validator.addMethod("pastdateonly", function (val, element, params) {
-		var value = $.trim($(element).val());
-		if (value === '') return true;
-
-		var maxDate = params.maxdate;
-
-		return Date(value) < Date(maxDate);
-	});
-
-	jQuery.validator.unobtrusive.adapters.add("pastdateonly", ["maxdate"],
-	  function (options) {
-	  	options.rules["pastdateonly"] = {
-	  		maxdate: options.params.maxdate
-	  	};
-	  	options.messages["pastdateonly"] = options.message;
-	  }
-	);
 });
+
+
+jQuery.validator.addMethod("pastdateonly", function (val, element, params) {
+	var value = $.trim($(element).val());
+	if (value === '') return true;
+
+	var maxDate = params.maxdate;
+
+	return Date(value) < Date(maxDate);
+});
+
+jQuery.validator.unobtrusive.adapters.add("pastdateonly", ["maxdate"],
+  function (options) {
+  	options.rules["pastdateonly"] = {
+  		maxdate: options.params.maxdate
+  	};
+  	options.messages["pastdateonly"] = options.message;
+  }
+);
