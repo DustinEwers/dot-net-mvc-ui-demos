e359ddd71b1a8563e266cc08d7ea758bd8ff85c8
Dustin Ewers <dustin.ewers@gmail.com>
2015-11-09 22:41:50 -0600

    Added some slide images. Also did some minor example refactoring.

--- ASPNET4/UIDemos/UIDemos/Helpers/HtmlHelpersExtensions.cs
+++ ASPNET4/UIDemos/UIDemos/Helpers/HtmlHelpersExtensions.cs
@@ -18,38 +18,6 @@ namespace UIDemos.Helpers
 		/// <param name="prop"></param>
 		/// <param name="containerClass"></param>
 		/// <returns></returns>
-		public static HtmlHelper<T> FluentTextEditBlockFor<T, TValue>(this HtmlHelper<T> helper,
-										  Expression<Func<T, TValue>> prop,
-										  string containerClass)
-		{
-			var modelData = ModelMetadata.FromLambdaExpression(prop, helper.ViewData);
-			var writer = helper.ViewContext.Writer;
-
-			var container = new TagBuilder("div");
-			container.AddCssClass("form-group");
-			container.AddCssClass(containerClass);
-
-			var label = helper.LabelFor(prop);
-
-			var text = (modelData.ModelType == typeof(DateTime) || modelData.ModelType == typeof(DateTime?)) ? helper.EditorFor(prop) : helper.TextBoxFor(prop, new { @class = "form-control" });
-			var validation = helper.ValidationMessageFor(prop);
-
-			container.InnerHtml = string.Format("{0} {1} {2}", label, text, validation);
-
-			writer.Write(container.ToString(TagRenderMode.Normal));
-
-			return helper;
-		}
-
-		/// <summary>
-		/// Creates a Bootstrap Form Field with a container, label, editor, and validation message
-		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <typeparam name="TValue"></typeparam>
-		/// <param name="helper"></param>
-		/// <param name="prop"></param>
-		/// <param name="containerClass"></param>
-		/// <returns></returns>
 		public static MvcHtmlString TextEditBlockFor<T, TValue>(this HtmlHelper<T> helper,
 										  Expression<Func<T, TValue>> prop,
 										  string containerClass)
